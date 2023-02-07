5f800d6ce79e1abbbf9437cb6d7d880ed9274217
rielgbrl <rielgasy9@gmail.com>
2023-02-07 11:43:19 +0700

    transform get post to async await

--- controllers/feed.js
+++ controllers/feed.js
@@ -7,29 +7,26 @@ const User = require('../models/user')
 
 const isAuth = require('../middleware/is-auth')
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
 
   const currentPage = req.query.page || 1
   const perPage = 2
   let totalItems
-  Post.find()
-    .countDocuments()
-    .then((count) => {
-      totalItems = count;
-      return Post.find().skip((currentPage - 1)*perPage).limit(perPage)
-    })
-    .then((posts) => {
-      res.status(200).json({
-        message: "Successfully get",
-        posts: posts, totalItems: totalItems
-      });
-    })
-    .catch((err) => {
+
+  try{
+    const count = await Post.find().countDocuments()
+    const posts = await Post.find().skip((currentPage - 1)*perPage).limit(perPage)
+    
+    res.status(200).json({
+      message: "Successfully get",
+      posts: posts, totalItems: count
+    });
+  }catch(err){
       if (!err.statusCode) {
         err.statusCode = 500;
       }
       next(err);
-    });
+  };
 
     
   
